72242a464f3e90a6126614f59511673d8a3d7b92
Rajdeep Bharati <rajdeepbharati13@gmail.com>
2018-12-29 16:37:24 +0530

    Added Redis Cache

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,3 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Caching
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+        "KEY_PREFIX": "example"
+    }
+}
